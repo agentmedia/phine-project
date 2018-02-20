1a6f82be67488dcaf82810bb438271e878fb0781
Klaus Potzesny <info@agent-media.de>
2018-02-20 14:18:56 +0100

    - removed unresolvable dependencies from composer event handlers

--- App/Phine/Project/Bootstrap.php
+++ App/Phine/Project/Bootstrap.php
@@ -1,10 +1,10 @@
 <?php
 namespace App\Phine\Project;
+use Composer\Script\Event;
+use Composer\Installer\PackageEvent;
 use Phine\Framework\System\IO\Folder;
 use Phine\Framework\System\IO\Path;
 use Phine\Framework\System\IO\File;
-use Composer\Script\Event;
-use Composer\Installer\PackageEvent;
 use Composer\Composer;
 /**
  * Bootstraps the project
@@ -40,11 +40,25 @@ class Bootstrap
     
     private static function writeBundleInfos(array $bundleInfos)
     {
-        File::CreateWithText(self::bundleInfosFile(), json_encode($bundleInfos));
+        file_put_contents(self::bundleInfosFile(), json_encode($bundleInfos));
     }
-    
+    private static function getSubFolders($dir) {
+        $handle = opendir($dir);
+        $result = array();
+        
+        $file = readdir($handle);
+        while ($file !== false)
+        {   
+            if ($file != '.' && $file != '..' && is_dir($file)) {
+                $result[] = $file;
+            }
+            $file = readdir($handle);
+        }
+        closedir($handle);
+        return $result;
+    }
     private static function addPackageBundleInfo($packagePath, array& $bundleInfos) {
-        $subFolders = Folder::GetSubFolders($packagePath);
+        $subFolders = self::getSubFolders($packagePath);
         if (count($subFolders) !== 1) {
             throw new \LogicException('Invalid Phine bundle found. It must contain a single directory with the name of the bundle)');
         }
@@ -52,6 +66,6 @@ class Bootstrap
         if (isset($bundleInfos[$name])) {
             throw new \LogicException("Ambiguous Phine bundle name '$name'");
         }
-        $bundleInfos[$name] = Path::Combine($packagePath, $name);
+        $bundleInfos[$name] = rtrim($packagePath, '/\\') . '/' . ltrim($name, '/\\');
     }
 }
